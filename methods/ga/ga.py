39bc91ddec238dd9c0dea967fab5749543f381d4
nguyenngocbaocmt02 <nguyenngocbaocmt@gmail.com>
2023-02-02 08:52:38 +0000

    update GA

---
+++ methods/ga/ga.py
@@ -0,0 +1,170 @@
+import numpy as np
+import random
+import copy
+
+class GA:
+    def __init__(self, pop_size, num_generations, mutation_probability, local_search_prob, keep_rate):
+        self.pop_size = pop_size
+        self.num_generations = num_generations
+        self.mutation_probability = mutation_probability
+        self.keep_rate = keep_rate
+        self.local_search_prob = local_search_prob
+
+    def decode(self, individual, N):
+        routes = []
+        tmp = []
+        for i in range(len(individual)):
+            if individual[i] >= N:
+                routes.append([0] + copy.deepcopy(tmp) + [0])
+                tmp = []
+            else:
+                tmp.append(individual[i])
+        routes.append([0] + copy.deepcopy(tmp) + [0])
+        return routes
+    
+    def encode(self, routes):
+        individual = []
+        num_cities = 0
+        for route in routes:
+            num_cities += (len(route) - 2)
+        for i, route in enumerate(routes):
+            for pos in route[1:-1]:
+                individual.append(pos)
+            if i != len(routes) -1 :
+                individual.append(i + 1 + num_cities)
+        return individual
+
+    def cost_route(self, distance_matrix, route):
+        tmp = 0
+        for i in range(0, len(route) - 1):
+            tmp += distance_matrix[route[i]][route[i+1]]
+        return tmp
+
+    def fitness_function_individual(self, distance_matrix, individual):
+        routes = self.decode(individual, len(distance_matrix))
+        fitness = -float("inf")
+        for route in routes:
+            fitness = max(self.cost_route(distance_matrix, route), fitness)
+        return fitness
+
+    def mutate(self, individual):
+        if random.uniform(0, 1) < self.mutation_probability:
+            start = random.randint(0, len(individual) - 1)
+            end = random.randint(0, len(individual) - 1)
+            new_individual = np.copy(individual)
+            new_individual[start], new_individual[end] = new_individual[end], new_individual[start]
+            return new_individual
+        else:
+            return individual
+
+    def ox_crossover(self, individual1, individual2):
+        # Select two random cut points for the OX crossover
+        cut1 = random.randint(0, len(individual1) - 1)
+        cut2 = random.randint(0, len(individual1) - 1)
+        # Ensure that cut1 is smaller than cut2
+        if cut1 > cut2:
+            cut1, cut2 = cut2, cut1
+        # Create the offspring using OX
+        offspring = [-1] * len(individual1)
+        offspring[cut1:cut2+1] = individual1[cut1:cut2+1]        
+        pos = (cut2 + 1) % len(individual1)
+        for i in range(len(individual1)):
+            if individual2[i] not in offspring:
+                while offspring[pos] != -1:
+                    pos = (pos + 1) % len(individual1)
+                offspring[pos] = individual2[i]
+        
+        return offspring
+
+    def generate_population(self, gen_len):
+        population = []
+        for i in range(self.pop_size):
+            population.append(np.random.permutation([_ for _ in range(1, gen_len+1)]))
+        return population
+
+    def select_parents(self, population, fitness_scores):
+        parent1 = random.choices(population, k=1)[0]
+        parent2 = random.choices(population, k=1)[0]
+        return parent1, parent2
+
+    def generate_offspring(self, individual1, individual2):
+        child = self.ox_crossover(individual1, individual2)
+        child = self.mutate(child)
+        return child
+
+    def two_opt(self, distance_matrix, individual):
+        routes = self.decode(individual, len(distance_matrix))
+        for stt, route in enumerate(routes):
+            for i in range(1, len(route) - 1):
+                for j in range(i+1, len(route) - 1):
+                    new_route = np.copy(route)
+                    new_route[i:j+1] = route[j:i-1:-1]
+                    new_cost = self.cost_route(distance_matrix, new_route)
+                    current_cost = self.cost_route(distance_matrix, route)
+                    if new_cost < current_cost:
+                        routes[stt] = new_route
+        return self.encode(routes)
+
+    def local_search_2(self, distance_matrix, individual):
+        routes = self.decode(individual, len(distance_matrix))
+        improve = True
+        max_stt, max_cost = None, -float("inf")
+        for stt, route in enumerate(routes):
+            tmp = self.cost_route(distance_matrix, route)
+            if tmp > max_cost:
+                max_cost = tmp
+                max_stt = stt
+        length = max_cost
+        for i in range(len(routes)):
+            if i == max_stt:
+                continue
+            new_routes = self.decode(individual, len(distance_matrix))
+            new_routes[max_stt], new_routes[i], new_length = self.optimize_routes(distance_matrix, new_routes[max_stt], new_routes[i])
+            if new_length < length:
+                return self.encode(new_routes)
+        return individual
+
+    def optimize_routes(self, distance_matrix, route1, route2):
+        max_length = max(self.cost_route(distance_matrix, route1), self.cost_route(distance_matrix, route2))
+        res_route1 = copy.deepcopy(route1[:])
+        res_route2 = copy.deepcopy(route2[:])
+        for i in range(1, len(route1)-1):
+            for j in range(1, len(route2)-1):
+                new_route1 = route1[:]
+                new_route2 = route2[:]
+                new_route2.insert(j, new_route1.pop(i))
+                new_length = max(self.cost_route(distance_matrix, new_route1), self.cost_route(distance_matrix, new_route2))
+                if new_length < max_length:
+                    res_route1 = new_route1
+                    res_route2 = new_route2
+                    max_length = new_length
+        return res_route1, res_route2, max_length
+
+    def solve(self, instance):
+        distance_matrix = instance.data["distance_matrix"]
+        K = instance.data["K"]
+        population = self.generate_population(len(distance_matrix) + K - 2)
+        best_individual = None
+        best_fitness = float('inf')
+        for generation in range(self.num_generations):
+            fitness_scores = [self.fitness_function_individual(distance_matrix, individual) for individual in population]
+            population_with_fitness = list(zip(population, fitness_scores))
+            population_with_fitness.sort(key=lambda x: x[1], reverse=False)
+            population = [route for route, fitness in population_with_fitness]
+            tmp = self.fitness_function_individual(distance_matrix, population[0])
+            if  tmp < best_fitness:
+                best_individual = population[0]
+                best_fitness = tmp
+            next_population = [population[i] for i in range(int(self.pop_size * self.keep_rate))]
+            while len(next_population) < self.pop_size:
+                parent1, parent2 = self.select_parents(population, fitness_scores)
+                child = self.generate_offspring(parent1, parent2)
+                next_population.append(child)
+            for i in range(len(next_population)):
+                if random.uniform(0, 1) < self.local_search_prob:
+                    next_population[i] = self.local_search_2(distance_matrix, next_population[i])
+                    next_population[i] = self.two_opt(distance_matrix, next_population[i])
+            population = next_population
+            print(generation, ":", best_fitness)
+        return self.decode(best_individual, len(distance_matrix)), best_fitness
+
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from methods.greedy.ortools_greedy import OrtoolsMTSP
 from utils.instance import Instance
+from methods.ga.ga import GA
 
-solver = OrtoolsMTSP()
 instance = Instance("")
 instance.create_example_data()
 
-print(solver.solve(instance))
+ga_model = GA(pop_size=200, num_generations=500, mutation_probability=0.2, local_search_prob=0.4, keep_rate=0.1)
+best_routes, best_fitness = ga_model.solve(instance)
+print("Best routes: ", best_routes)
+print("Best distance: ", best_fitness)
+print("Check distance:", instance.obj_func(best_routes))
